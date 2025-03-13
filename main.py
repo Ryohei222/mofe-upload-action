617eecf1bc851ac77911e7ec82478ece122f0e4a
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-03-14 01:59:06 +0900

    ログを追加

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
+from logging import getLogger
 from pathlib import Path
 
 from mofeapi.client import Client
 
 from upload import upload_sample_explanations, upload_testcases
-from util import build_problem_params, compress_testcases, load_problem_toml, load_statement
+from util import build_problem_params, compress_testcases, load_problem_toml, load_statement, log_problem_diff
+
+logger = getLogger(__name__)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -16,12 +19,21 @@ if __name__ == "__main__":
     parser.add_argument(
         "--force-upload-statement", type=str, help="問題文を強制的にアップロードするかどうか", default="false"
     )
+    parser.add_argument("--use-debug-logging", type=str, help="デバッグログを出力するかどうか", default="false")
 
     args = parser.parse_args()
 
     flag_upload_testcases = args.upload_testcases.lower() == "true"
     flag_upload_statement = args.upload_statement.lower() == "true"
     flag_force_upload_statement = args.force_upload_statement.lower() == "true"
+    flag_use_debug_logging = args.use_debug_logging.lower() == "true"
+
+    if flag_use_debug_logging:
+        getLogger().setLevel("DEBUG")
+
+    logger.debug("upload-testcases: %s", flag_upload_testcases)
+    logger.debug("upload-statement: %s", flag_upload_statement)
+    logger.debug("force-upload-statement: %s", flag_force_upload_statement)
 
     client = Client()
     client.login(args.mofe_username, args.mofe_password)
@@ -31,6 +43,8 @@ if __name__ == "__main__":
         if not path.is_dir() or not (path / "problem.toml").exists():
             continue
 
+        logger.info('Processing "%s"', path)
+
         # problem.toml を読み込む
         problem_config = load_problem_toml(path / "problem.toml")
         problem_id = problem_config.problem_id
@@ -38,7 +52,6 @@ if __name__ == "__main__":
 
         # 問題文をアップロードする場合
         if flag_upload_statement:
-            # 問題文を読み込む
             statement = load_statement(path / "ss-out" / f"{position_in_contest}.md")
             problem_params = build_problem_params(statement, problem_config)
 
@@ -49,6 +62,8 @@ if __name__ == "__main__":
                     raise ValueError("MOFE 上の問題名と問題文の問題名が一致しません")
 
             client.update_problem(problem_id, problem_params)
+            updated_problem = client.get_problem(problem_id)
+            log_problem_diff(problem_on_mofe, updated_problem)
 
         # テストケースをアップロードする場合
         if flag_upload_testcases:
--- util.py
+++ util.py
@@ -1,9 +1,12 @@
 import os
 import shutil
+from logging import getLogger
 from parser import ProblemConfig, Statement, parse_problem_toml, parse_statement_md
 from pathlib import Path
 
-from mofeapi.models.problem import ProblemParams
+from mofeapi.models.problem import ProblemDetail, ProblemParams
+
+logger = getLogger(__name__)
 
 
 def compress_testcases(problem_path: Path) -> None:
@@ -52,3 +55,23 @@ def load_statement(statement_md_path: Path) -> Statement:
         raise FileNotFoundError(f"statement.md not found in {statement_md_path}")
     statement_md = open(statement_md_path, "r").read()
     return parse_statement_md(statement_md)
+
+
+def log_problem_diff(old_problem: ProblemDetail, new_problem: ProblemDetail) -> None:
+
+    def log_diff(field_name: str, old_value, new_value):
+        if old_value != new_value:
+            logger.info(f"{field_name} が更新されました")
+            logger.info(f"old: {old_value}")
+            logger.info(f"new: {new_value}")
+
+    log_diff("問題名", old_problem.name, new_problem.name)
+    log_diff("問題文", old_problem.statement, new_problem.statement)
+    log_diff("制約", old_problem.constraints, new_problem.constraints)
+    log_diff("入力形式", old_problem.input_format, new_problem.input_format)
+    log_diff("出力形式", old_problem.output_format, new_problem.output_format)
+    log_diff("部分点", old_problem.partial_scores, new_problem.partial_scores)
+    log_diff("難易度", old_problem.difficulty, new_problem.difficulty)
+    log_diff("実行時間制限", old_problem.execution_time_limit, new_problem.execution_time_limit)
+    log_diff("1 つ前からの提出制限", old_problem.submission_limit_1, new_problem.submission_limit_1)
+    log_diff("2 つ前からの提出制限", old_problem.submission_limit_2, new_problem.submission_limit_2)
